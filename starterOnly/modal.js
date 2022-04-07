f5250d8ece0cd649697dd1cb0592f09d79c1c277
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-04-07 03:19:09 +0200

    mise à jour des fichiers Modal et HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
 const modalCloseButton = document.querySelector(".close");
 
 const form = document.getElementById("form");
@@ -28,6 +29,8 @@ modalCloseButton.addEventListener("click", () => {
   modalbg.style.display = "none";
 });
 
+/* L'action par défaut du formulaire ne soit pas pris en compte par le navigateur 
+en utilisant la méthode preventDefault */
 const validateForm = (event) => {
   event.preventDefault();
 
@@ -41,6 +44,7 @@ const validateForm = (event) => {
    * - boutons radios
    * - bouton des CGU
    */
+
   const firstNameInput = document.getElementById("first");
   const lastNameInput = document.getElementById("last");
   const emailInput = document.getElementById("email");
@@ -55,7 +59,7 @@ const validateForm = (event) => {
 
   /* Regex */
   const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim;
-  const quantityRegex = /\d+/;
+  const quantityRegex = /\d+/; // Nombre uniquement
 
   /**
    * récupérer les valeurs des champs dans un tableau d'objets "data"
@@ -77,46 +81,75 @@ const validateForm = (event) => {
     formData.setAttribute("data-error-visible", false);
     formData.setAttribute("data-error", "");
   });
-
+  // Affiche les messages d'erreurs si condition pas respecté
   const setError = (inputElement, errorMessage) => {
-    inputElement === locationRadios ? (
-      inputElement[0].parentNode.setAttribute("data-error-visible", true),
-      inputElement[0].parentNode.setAttribute("data-error", errorMessage)
-    ) : (
-      inputElement.parentNode.setAttribute("data-error-visible", true),
-      inputElement.parentNode.setAttribute("data-error", errorMessage)
-    )
+    inputElement === locationRadios
+      ? (inputElement[0].parentNode.setAttribute("data-error-visible", true),
+        inputElement[0].parentNode.setAttribute("data-error", errorMessage))
+      : (inputElement.parentNode.setAttribute("data-error-visible", true),
+        inputElement.parentNode.setAttribute("data-error", errorMessage));
   };
-
-  data.firstName.length < 2 ? (setError(firstNameInput, "Veuillez entrer 2 caractères ou plus pour le champ du nom."), errors["firstName"] = true) : null;
-  data.lastName.length < 2 ? (setError(lastNameInput, "Veuillez entrer 2 caractères ou plus pour le champ du prénom."), errors["lastName"] = true) : null;
-  !data.email.match(emailRegex) ? (setError(emailInput, "Veuillez entrer une adresse email valide."), errors["email"] = true) : null;
-  !data.birthdate ? (setError(birthdateInput, "Vous devez entrer votre date de naissance."), errors["birthdate"] = true) : null;
-  !data.quantity.match(quantityRegex) ? (setError(quantityInput, "Veuillez entrer un nombre valide."), errors["quantity"] = true) : null;
-  checked.length < 1 ? (setError(locationRadios, "Vous devez choisir une option."), errors["location"] = true) : null;
-  !touCheckbox.checked ? (setError(touCheckbox, "Vous devez vérifier que vous acceptez les termes et conditions."), errors["tos"] = true) : null;
-  console.log(Object.keys(errors).length)
+  // Des conditions Ternaires
+  data.firstName.length < 2
+    ? (setError(
+        firstNameInput,
+        "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+      ),
+      (errors["firstName"] = true))
+    : null;
+  data.lastName.length < 2
+    ? (setError(
+        lastNameInput,
+        "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+      ),
+      (errors["lastName"] = true))
+    : null;
+  !data.email.match(emailRegex)
+    ? (setError(emailInput, "Veuillez entrer une adresse email valide."),
+      (errors["email"] = true))
+    : null;
+  !data.birthdate
+    ? (setError(birthdateInput, "Vous devez entrer votre date de naissance."),
+      (errors["birthdate"] = true))
+    : null;
+  !data.quantity.match(quantityRegex)
+    ? (setError(quantityInput, "Veuillez entrer un nombre valide."),
+      (errors["quantity"] = true))
+    : null;
+  checked.length < 1
+    ? (setError(locationRadios, "Vous devez choisir une option."),
+      (errors["location"] = true))
+    : null;
+  !touCheckbox.checked
+    ? (setError(
+        touCheckbox,
+        "Vous devez vérifier que vous acceptez les termes et conditions."
+      ),
+      (errors["tos"] = true))
+    : null;
+  console.log(Object.keys(errors).length);
   if (Object.keys(errors).length === 0) {
     confirmationForm();
   }
 };
 
 const confirmationForm = () => {
-  console.log("test")
-  const modalBody = document.querySelector('.modal-body');
-  form.style.display = 'none';
+  const modalBody = document.querySelector(".modal-body");
+  form.style.display = "none";
 
-  const confirmationTitle = document.createElement('h3');
+  const confirmationTitle = document.createElement("h3");
   confirmationTitle.textContent = "Merci pour votre inscription";
-  confirmationTitle.style.textAlign = 'center';
-  confirmationTitle.style.margin = '16px 0';
+  confirmationTitle.style.textAlign = "center";
+  confirmationTitle.style.margin = "16px 0";
 
-  const confirmationButton = document.createElement('button');
-  confirmationButton.classList.add('button');
-  confirmationButton.classList.add('btn-submit');
+  const confirmationButton = document.createElement("button");
+  confirmationButton.classList.add("button");
+  confirmationButton.classList.add("btn-submit");
   confirmationButton.textContent = "Fermer";
-  confirmationButton.addEventListener('click', () => { form.submit() });
-
+  confirmationButton.addEventListener("click", () => {
+    form.submit();
+  });
+  // On insère des éléments dans le modalBody
   modalBody.appendChild(confirmationTitle);
   modalBody.appendChild(confirmationButton);
-}
+};
